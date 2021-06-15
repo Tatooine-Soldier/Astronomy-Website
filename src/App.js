dc415d020291113b45f0a08c87dc4eb22291b7fd
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-06-15 14:46:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+          <Nav/>
+          <Switch>
+            <Route path="/" exact/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
