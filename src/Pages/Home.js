dcf312066fa87aff573ab796a9484d71ccba130c
Tatooine-Soldier <58118455+Tatooine-Soldier@users.noreply.github.com>
2021-06-16 19:51:09 +0100

    Add files via upload
    Fetched the ISS latitude and longitude data using fetch api in Home.js. Each time button clicked, the co-ordinates appear in the centre of the screen

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,18 +2,11 @@ import {React, useState} from 'react'
 
 export default function Home() {
 
+    const [mydata, setMydata] = useState({})
     const handleFind = () => {
-        fetch("http://api.open-notify.org/iss-now.json", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                message: 'w'
-            })
-        })
-            .then(res => {return res.json()})
-            .then(data => console.log(data))
+        fetch("http://api.open-notify.org/iss-now.json")
+            .then(res => {if (res.ok) {return res.json()}})
+            .then(data => setMydata(data))
             .catch(error => console.log('error'))
     }
 
@@ -21,6 +14,15 @@ export default function Home() {
         <div className="main">
             <section className="sidebar">
                 <h2>Where is the ISS now?</h2><button onClick={handleFind}>FIND ISS</button>
+                <section className="data-container">
+                    <div>
+                    </div>
+                    <ul>
+                        <li>Latitude: {mydata.iss_position.latitude}</li>
+                        <li>Longitude: {mydata.iss_position.longitude}</li>
+                        {console.log(mydata.iss_position.latitude)}
+                    </ul>
+                </section>
             </section>
             <section className="content"></section>
         </div>
